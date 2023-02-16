665f4da122a7e23675ac8eb78283c2ad5ab91cfa
dsavvin <dmsavvin@gmail.com>
2023-02-16 20:06:29 +0300

    Fix ex. 1.5

--- solutions/1.1/e-1.5.rkt
+++ solutions/1.1/e-1.5.rkt
@@ -6,14 +6,14 @@
 ;faced with is using applicativeorder evaluation or normal-order evaluation.
 ;He defines the following two procedures:
 
-(define (p) (p))
+;(define (p) (p))
 
-(define (test x y)
-  (if (= x 0) 0 y))
+;(define (test x y)
+;  (if (= x 0) 0 y))
 
 ;Then he evaluates the expression
 
-(test 0 (p))
+;(test 0 (p))
 
 ;What behavior will Ben observe with an interpreter that uses applicative-order
 ;evaluation? What behavior will he observe with an interpreter that uses normal-
